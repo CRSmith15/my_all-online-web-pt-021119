4d9d30c510465cb73d6f121b1077abe29d3f5ab0
Chance Smith <chancesmith97@hotmail.com>
2019-02-22 22:55:25 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_value = []
   while i < collection.length
-    yeild(collection)
+    block_return_value << yeild(collection)
     i = i + 1
+  end
+  if block_return_value.include?(false)
+    false
+  else 
+    true 
   end
 
 end
